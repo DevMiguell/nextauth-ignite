e515232ac960f68aae24c6f324d5e3aa33bf2476
DevMiguell <buguel2000@gmail.com>
2021-05-31 10:15:12 -0300

    criando component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 
@@ -9,6 +10,10 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+   roles: ['administrator', 'editor']
+  })
+
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
@@ -16,7 +21,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   )
 }
 
